51d994dbc862e511b02b014e728646f69b1b37e7
Vengadachalapathi A <144203867+Akvk33@users.noreply.github.com>
2025-12-12 18:22:12 +0530

    Add files via upload

--- assests/script.js
+++ assests/script.js
@@ -1,22 +1,27 @@
-const notesArea = `<div class="notesArea col-md-3 px-0">
-                <div class="notesContent" contenteditable="true" >Enter Your Notes Here...</div>
-                <div class="notesBottom d-flex justify-content-center dragHandle">
-                    <i class="bi bi-trash3-fill deleteBtn text-danger btn btn-secondary"></i>
+const notesArea =
+    `<div class="notesArea col-md-3 px-0">
+                <div class="notesContent" contenteditable="true">Enter Your Notes Here...</div>
+                <div class="notesBottom d-flex justify-content-center py-3">
                 </div>
-            </div>`
+                <i class="bi bi-trash-fill deleteBtn dragHandle p-0"></i>
+                <i class="bi bi-arrows-move moveBtn dragHandle p-0"></i>
+                <i class="bi bi-arrows-angle-expand dragHandle zoomBtn p-0 "></i>
+            </div>
+            `
 
 $(document).click(function (event) {
     const tar = event.target;
     if (tar.classList.contains('add')) {
         const $note = $(notesArea).hide().appendTo(".heroBody").show("clip", 500, function () {
             $(this).draggable({
-                cancel: ".notesContent,.deleteBtn",
+                cancel: ".notesContent,.deleteBtn,.notesBottom,.zoomBtn",
                 containment: ".heroBody",
-                cursor: "move",
                 revert: "invalid"
             });
         });
+
     }
+
     if (tar.closest(".notesContent")) {
         $('.notesArea').removeClass('bord');
         $(tar.closest(".notesArea")).addClass('bord');
@@ -32,19 +37,25 @@ $(document).click(function (event) {
         $(tar.closest('.notesArea')).hide("clip", 1000, callback);
     }
 });
+
 $('#exampleModal').click(function (e) {
     e.preventDefault();
     if (e.target.classList.contains('del')) {
         function callback() {
             $(this).remove()
         }
-        $(".notesArea")/* .slideUp(300).delay(800).fadeOut(100, function () {
-            $(this).remove();
-        }); */.hide("pulsate", 1000, callback);
-
-
+        $(".notesArea").hide("pulsate", 1000, callback);
     }
 });
-/* $(document).on("click", function (event) {
-    
-}); */
+
+$(document).on("click", ".zoomBtn", function (event) {
+    const $note = $(this).closest(".notesArea");
+    const $clone = $note.clone(true, true);
+    $clone.find(".deleteBtn, .moveBtn,.zoomBtn,.notesBottom,.ui-draggable").remove();
+    $clone.removeClass("col-md-3");
+    $clone.addClass("zoomed-note");
+    $clone.find('.notesContent').removeAttr("contenteditable");
+    $(".zoomContent").empty().append($clone);
+    const modal = new bootstrap.Modal(document.getElementById("zoomModal"));
+    modal.show();
+});
